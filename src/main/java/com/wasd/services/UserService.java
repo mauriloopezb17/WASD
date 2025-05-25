9719a76c558706675e4718820eb654f849c346c0
Mauricio Lopez <mauriloopezb17@gmail.com>
2025-05-25 18:46:14 -0400

    cambios en la clase UserService para que nos devuelva las clases correspondientes de Users

--- src/main/java/com/wasd/services/UserService.java
+++ src/main/java/com/wasd/services/UserService.java
@@ -2,11 +2,20 @@ package com.wasd.services;
 
 import com.wasd.utils.PasswordHashUtil;
 import com.wasd.database.UserDAO;
+import com.wasd.database.PlayerDAO;
+import com.wasd.database.PublisherDAO;
+import com.wasd.database.AdminDAO;
 import com.wasd.models.User;
+import com.wasd.models.Player;
+import com.wasd.models.Admin;
+import com.wasd.models.Publisher;
 
 public class UserService {
 
     private final UserDAO dao = new UserDAO();
+    private final PlayerDAO dao2 = new PlayerDAO();
+    private final PublisherDAO dao3 = new PublisherDAO();
+    private final AdminDAO dao4 = new AdminDAO();
     private final PasswordHashUtil passwordHashUtil = new PasswordHashUtil();
 
     // validacion para registrar un nuevo usuario
@@ -44,6 +53,51 @@ public class UserService {
         if (passwordHashUtil.verifyPassword(user.getPassword(), inputPassword)) {
             System.out.println("Login exitoso: " + user.getUsername());
             return user;
+        } else {
+            System.out.println("Incorrect password");
+            return null;
+        }
+    }
+
+    public Player loginPlayer(String input, String inputPassword) {
+
+        User user = dao.searchUser(input);
+        Player player = dao2.searchPlayer(user.getIdUser());
+
+        // Validacion de la contrasena
+        if (passwordHashUtil.verifyPassword(player.getPassword(), inputPassword)) {
+            System.out.println("Login exitoso: " + user.getUsername());
+            return player;
+        } else {
+            System.out.println("Incorrect password");
+            return null;
+        }
+    }
+
+    public Publisher loginPublisher(String input, String inputPassword) {
+
+        User user = dao.searchUser(input);
+        Publisher publisher = dao3.searchPublisher(user.getIdUser());
+
+        // Validacion de la contrasena
+        if (passwordHashUtil.verifyPassword(publisher.getPassword(), inputPassword)) {
+            System.out.println("Login exitoso: " + user.getUsername());
+            return publisher;
+        } else {
+            System.out.println("Incorrect password");
+            return null;
+        }
+    }
+
+    public Admin loginAdmin(String input, String inputPassword) {
+
+        User user = dao.searchUser(input);
+        Admin admin = dao4.searchAdmin(user.getIdUser());
+
+        // Validacion de la contrasena
+        if (passwordHashUtil.verifyPassword(admin.getPassword(), inputPassword)) {
+            System.out.println("Login exitoso: " + user.getUsername());
+            return admin;
         } else {
             System.out.println("Incorrect password");
             return null;
